e057f69b5d2185fc6921105e9accd67dcd9d767e
Mesi21 <mse212002@gmail.com>
2020-10-02 17:25:30 +0300

    :sparkles: added stickler and rubocop

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,11 @@
 class MessagesController < ApplicationController
+    before_action :find_message, only: [:show, :edit, :update, :destroy]
+    
     def index
-        @messages = Message.all.order('created_at DESC')
-    end
+		@messages = Message.all.order("created_at DESC")
+	end
 
     def show
-        @message = Message.find(params[:id])
     end
 
     def new
@@ -21,9 +22,22 @@ class MessagesController < ApplicationController
         end
     end
 
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
     private
 
     def message_params
         params.require(:message). permit(:title, :description)
+    end
+
+    def find_message
+        @message = Message.find(params[:id])
     end
 end
