c7aa3ab4d88904d27a49508df16fcfa51a807b98
stickler-ci <support@stickler-ci.com>
2020-10-02 16:49:08 +0000

    Fixing style errors.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,50 +1,50 @@
+# frozen_string_literal: true
+
 class MessagesController < ApplicationController
-    before_action :find_message, only: [:show, :edit, :update, :destroy]
-    
-    def index
-		@messages = Message.all.order("created_at DESC")
-	end
+  before_action :find_message, only: %i[show edit update destroy]
 
-    def show
+  def index
+    @messages = Message.all.order('created_at DESC')
+  end
+
+  def show; end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+
+    if @message.save
+      redirect_to root_path
+    else
+      render 'new'
     end
+  end
 
-    def new
-        @message = Message.new
+  def edit; end
+
+  def update
+    if @message.update(message_params)
+      redirect_to message_path
+    else
+      render 'edit'
     end
+  end
 
-    def create
-        @message = Message.new(message_params)
+  def destroy
+    @message.destroy
+    redirect_to root_path
+  end
 
-        if @message.save
-            redirect_to root_path
-        else
-            render 'new'
-        end
-    end
+  private
 
-    def edit
-    end
+  def message_params
+    params.require(:message). permit(:title, :description)
+  end
 
-    def update
-        if @message.update(message_params)
-            redirect_to message_path
-        else
-            render 'edit'
-        end
-    end
-
-    def destroy
-        @message.destroy
-        redirect_to root_path
-    end
-
-    private
-
-    def message_params
-        params.require(:message). permit(:title, :description)
-    end
-
-    def find_message
-        @message = Message.find(params[:id])
-    end
+  def find_message
+    @message = Message.find(params[:id])
+  end
 end
