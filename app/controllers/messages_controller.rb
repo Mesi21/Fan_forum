a0cab7b8dfd13d5e0ba676ee9ee6a40f12fd5718
Mesi21 <mse212002@gmail.com>
2020-10-02 19:44:35 +0300

    :art: added view file for edit, edit and delete actions in the controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -26,9 +26,16 @@ class MessagesController < ApplicationController
     end
 
     def update
+        if @message.update(message_params)
+            redirect_to message_path
+        else
+            render 'edit'
+        end
     end
 
     def destroy
+        @message.destroy
+        redirect_to root_path
     end
 
     private
